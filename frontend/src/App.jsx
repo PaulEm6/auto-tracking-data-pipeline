e4b80166f0fd9d83e6ed9dd45323462811ac531c
paulem6 <paulemmanuelatte12@gmail.com>
2026-02-28 16:58:56 -0500

    working on frontend, two simple boxes blue tints

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-900">
+      <div className="flex space-x-4">
+        <div className="w-32 h-32 bg-blue-700" />
+        <div className="w-32 h-32 bg-blue-600" />
+      </div>
+    </div>
+  )
+}
+
+export default App
